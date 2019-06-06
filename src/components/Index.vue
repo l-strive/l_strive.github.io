5a4cd0a4d7615f2e059c9731e9e47457ff3c4dc1
Beginner <l_strive@163.com>
2019-06-06 16:45:14 +0800

    添加导航

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <ul>
+      <li>首页</li>
+      <li>沸点</li>
+      <li>话题</li>
+      <li>小册</li>
+      <li>活动</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  ul{ list-style: none;}
+  #nav{ height: 40px; line-height: 40px;}
+  #nav ul{ display: flex; height: 40px;}
+  #nav ul li{ flex:1;}
+</style>
