b1d50b58d93f0dc9b94ec1aca7837e9dec87db34
Beginner <l_strive@163.com>
2020-04-21 19:48:13 +0800

    Update rem.js

--- public/static/js/rem.js
+++ public/static/js/rem.js
@@ -1,15 +1,16 @@
 /*
 **author:Beginner
-**create:20190606
+**create:20180901
+**revamp:20200419
 **email:BeginnerMind@163.com
 */
 var vRem;
 (function(win) {
-    var def_w=720,def_px=100,doc = win.document,docEl = doc.documentElement,
+    var def_w=375,def_px=10,max_w=1024,doc = win.document,docEl = doc.documentElement,
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     recalc=function(){
         var width = docEl.getBoundingClientRect().width;
-        width=width>720?720:width;
+        width=width>max_w?max_w:width;
         vRem=Math.floor(width/def_w*def_px);
         document.getElementsByTagName("html")[0].style.cssText = 'font-size: ' + vRem +"px";
     }
